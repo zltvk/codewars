501b4930bbd712912f69fef670d028902d91c54d
zltvk <o.zlotovik@gmail.com>
2025-08-14 19:57:06 +0200

    v4.1_final version

--- mumbling.py
+++ mumbling.py
@@ -6,16 +6,21 @@ accum("cwAt") -> "C-Ww-Aaa-Tttt"
 '''
 
 def accum(st):
-    result_st = ''
+    multiplier = 1
+    result_st = []
     for i in st:
-        st_index = int(st.index(i))
-        part = i.lower() * (st_index+1)
-        result_st += part.capitalize() + '-'
+        part = i * (multiplier)
+        result_st.append(part.capitalize())
+        multiplier += 1
+    return '-'.join(result_st)
         
-    return result_st[:-1]
-        
 
 
 
-print(accum("RqaEzty"))
+print(accum("NyffsGeyylB"))
 
+assert accum("ZpglnRxqenU") == "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+assert accum("NyffsGeyylB") == "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+assert accum("MjtkuBovqrU") == "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+assert accum("EvidjUnokmM") == "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+assert accum("HbideVbxncC") == "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
